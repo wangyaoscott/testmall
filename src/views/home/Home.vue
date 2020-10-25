f7e477c2f0feb3ecb879e7e9e1163ab558b2aecb
wangyaoscott <wangyaoscott@163.com>
2020-10-25 23:34:32 +0800

    提交首页布局

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,40 +1,118 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">Shopping It</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">Shopping It</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control
+      class="tab-control"
+      :titles="titles"
+      @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
   </div>
 </template>
 
 <script>
+  // 导入当前路径下的组件
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+  import FeatureView from "./childComps/FeatureView";
+  // 导入全局组件
   import NavBar from "components/common/navbar/NavBar";
-  import {getHomeMultidata} from 'network/home'
+  import TabControl from 'components/content/tabcontrol/TabControl'
+  import GoodsList from 'components/content/goods/GoodsList'
+  // 导入方法
+  import {getHomeMultidata, getHOmeGoods} from 'network/home'
+
   export default {
     name: "Home",
     components: {
-      NavBar
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl,
+      GoodsList
     },
     data() {
       return {
         result: null,
         banners: [],
-        recommends: []
+        recommends: [],
+        titles: ['流行', '新款', '精选'],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop'
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
       }
     },
     created() {
-      getHomeMultidata().then(res => {
-        console.log(res);
-        this.banners = res.data.banner.list
-        this.recommends = res.data.recommend.list
-      })
+      this.getHomeMultiData()
+      this.getHOmeGoods('pop')
+      this.getHOmeGoods('new')
+      this.getHOmeGoods('sell')
+
+    },
+    methods: {
+      getHomeMultiData() {
+        getHomeMultidata().then(res => {
+          // console.log(res);
+          this.banners = res.data.banner.list
+          this.recommends = res.data.recommend.list
+        })
+      },
+      getHOmeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHOmeGoods(type, page).then(res => {
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1
+          console.log(this.goods);
+        })
+      },
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #ffffff;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item.id">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+  export default {
+    props: {
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    name: "HomeSwiper",
+    components: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
